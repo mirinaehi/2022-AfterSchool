b32b7f27d9f72e5b3e1739eb9e06ec96acf969c9
ASJW <AS@JW>
2023-01-03 15:21:47 +0900

    방향에 따라 좌우로 움직임

--- Moonbub/Animation.cpp
+++ Moonbub/Animation.cpp
@@ -8,8 +8,10 @@ struct Player {
 	int idx;					// 애니메이션 index
 	int frames;					// 애니메이션 frame수
 	long ani_time;				// 애니메이션이 바뀔 때의 시각
-	long ani_delay;				// 화면 전환의 텀 
-};
+	long ani_delay;				// 화면 전환의 텀
+	int speed;
+};	
+
 
 int main(void)
 {
@@ -38,6 +40,7 @@ int main(void)
 	player.sprite.setSize(Vector2f(90, 120));
 	player.sprite.setPosition(200, 400);
 	player.ani_delay = 1000 / player.frames / 2;		// 0.5초바다 걸음
+	player.speed = 5;
 
 	start_time = clock();
 	player.ani_time = start_time;
@@ -56,6 +59,17 @@ int main(void)
 			default:
 				break;
 			}
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			player.sprite.setScale(1, 1);
+			player.sprite.move(player.speed, 0);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			player.sprite.setScale(-1, 1);
+			player.sprite.move(-player.speed, 0);
 		}
 
 		// 0.1초마다 애니메이션 그림이 바뀜
