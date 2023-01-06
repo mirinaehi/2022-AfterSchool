14eb2633cb65015798c4b4a0b028d95422263218
ASJW <AS@JW>
2023-01-06 13:43:59 +0900

    INSERT

--- mysql_ex/main.c
+++ mysql_ex/main.c
@@ -14,10 +14,23 @@ int main(void)
 		, DB_NAME, 3306, NULL, 0);
 	
 	if (connection == NULL)
-		printf("DB 연결 실패");
+		printf("DB 연결 실패\n");
 	else
-		printf("DB 연결 성공");
+		printf("DB 연결 성공\n");
 
+
+	
+	int query_result;
+	// INSERT
+	char insert_q[50] = "INSERT INTO korea VALUES('TOKO', 10000)";
+	
+	// Query 실행
+	query_result = mysql_query(connection, insert_q);
+
+	if (query_result == 0)
+		printf("%s 성공\n", insert_q);
+	else
+		printf("%s 실패\n", insert_q);
 	
 	
 	mysql_close(&mysql);	// mysql 종료
