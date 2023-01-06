2d7671199cbe7fc7b03f49e823ab41f56e7015b1
ASJW <AS@JW>
2023-01-06 13:32:16 +0900

    mysql 연결

---
+++ mysql_ex/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <mysql/mysql.h>	// libmysql:x64-windows
+#include "db_config.h"		// mysql 계정정보
+
+#define TRUE 1
+#define FALSE 0
+
+int main(void)
+{
+	MYSQL mysql;
+	mysql_init(&mysql);
+	// mysql 연결시도, 실패시 NULL 반환
+	MYSQL* connection = mysql_real_connect(&mysql, DB_HOST, DB_USER, DB_PASS
+		, DB_NAME, 3306, NULL, 0);
+	
+	if (connection == NULL)
+		printf("DB 연결 실패");
+	else
+		printf("DB 연결 성공");
+
+	
+	
+	mysql_close(&mysql);	// mysql 종료
+
+}
